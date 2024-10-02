21cf876aeaab469fb5cf61a500332f8888e14bf0
Felix Nehrke <felix@nehrke.info>
2024-10-02 02:41:04 +0200

    Setup the app to run via lambda@edge in cloudfront

--- main.tf
+++ main.tf
@@ -1,6 +1,92 @@
 output "demo_url" {
   value = aws_lambda_function_url.this
 }
+output "demo_url2" {
+  value = aws_cloudfront_distribution.this
+}
+
+resource "aws_s3_bucket" "this" {
+  // "bucket" is intentionally not set to guarantee a unique name
+  tags = {
+    Name = "nuxt demo"
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.this.arn]
+    }
+  }
+}
+
+
+locals {
+  s3_origin_id = "nuxtDemoS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "this" {
+  enabled         = true
+  is_ipv6_enabled = true
+  origin {
+    domain_name              = aws_s3_bucket.this.bucket_regional_domain_name
+    origin_id                = local.s3_origin_id
+    origin_access_control_id = aws_cloudfront_origin_access_control.this.id
+  }
+  default_cache_behavior {
+    allowed_methods        = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
+    cached_methods         = ["HEAD", "GET", "OPTIONS"]
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 30
+    max_ttl                = 60
+    compress               = true
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+    lambda_function_association {
+      event_type = "origin-request"
+      lambda_arn = aws_lambda_function.this.qualified_arn
+    }
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "this" {
+  name                              = "nuxt-demo"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
 
 resource "aws_lambda_permission" "this" {
   statement_id           = "FunctionUrlInvokePermission"
@@ -16,12 +102,16 @@ resource "aws_lambda_function_url" "this" {
 }
 
 resource "aws_lambda_function" "this" {
+  provider         = aws.us_east_1
   filename         = data.archive_file.this.output_path
   function_name    = "nuxt-demo"
-  handler          = "index.handler"
+  handler          = "wrapper.handler"
   role             = aws_iam_role.this.arn
   source_code_hash = data.archive_file.this.output_base64sha256
   runtime          = "nodejs18.x"
+  timeout          = 5
+  memory_size      = 128
+  publish          = true
 }
 
 data "archive_file" "this" {
@@ -39,8 +129,11 @@ data "aws_iam_policy_document" "this" {
   statement {
     effect = "Allow"
     principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
+      type = "Service"
+      identifiers = [
+        "lambda.amazonaws.com",
+        "edgelambda.amazonaws.com"
+      ]
     }
     actions = ["sts:AssumeRole"]
   }
@@ -57,4 +150,8 @@ terraform {
 
 provider "aws" {
   region = "eu-central-1"
+}
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us_east_1"
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
+import { cpSync } from 'node:fs';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   nitro: {
     preset: 'aws_lambda',
+  },
+   hooks: {
+    'nitro:build:public-assets': (nitro) => {
+      // copy email templates to .output/server/emails
+      const targetDir = nitro.options.output.serverDir;
+      cpSync('./lambda@edge', targetDir, { recursive: true });
+    }
   }
 })
